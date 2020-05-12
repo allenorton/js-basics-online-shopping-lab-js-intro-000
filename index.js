a264d1866b9fb910e40791bf56cc3c553165aede
Elizabeth Allene Norton <norton.allene@gmail.com>
2020-05-12 22:56:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,7 +51,10 @@ function removeFromCart(item) {
       return cart
     }
   }
-  if 
+  if (item != cart[item]) {
+    return "That item is not in your cart."
+
+  }
 }
 
 function placeOrder(cardNumber) {
